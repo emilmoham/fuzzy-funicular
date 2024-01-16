120ed6cd01840d034edc986ec8642f44de97ffa3
Emil Mohammed <emilmoham@gmail.com>
2024-01-15 20:15:25 -0500

    Initial Commit

---
+++ JenkinsTesting/Controllers/VersionController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JenkinsTesting.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VersionController : Controller
+    {
+        public IConfiguration _config;
+
+        public VersionController(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult IsAlive()
+        {
+            string env = _config.GetValue<string>("where");
+            string version = _config.GetValue<string>("version");
+            return Ok($"Is Alive -- {version} -- {env}");
+        }
+    }
+}
